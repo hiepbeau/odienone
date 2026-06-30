9879f14a19ab59f158992f3ee700b6ce5a50eaee
hiepnguyen <hiepnguyenkhtn@gmail.com>
2026-06-30 17:28:09 +0700

    fix ES Module

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["firebase-admin"],
+  serverExternalPackages: ["firebase-admin", "jwks-rsa", "jose"],
   experimental: {
     serverActions: {
       bodySizeLimit: "6mb",
